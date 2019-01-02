afc53408115c3f9e0e8dc27cf5deed0c2830342a
Shubhadeep <41886925+shubha161@users.noreply.github.com>
2019-01-03 02:16:46 +0530

    Update mac_ch.py

--- mac_ch.py
+++ mac_ch.py
@@ -1,18 +1,16 @@
 import subprocess
 import optparse
 
+def mac_change(interface, new_mac):
+    print("[+] Your " + interface + " is going to change with " + new_mac)
+
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+    subprocess.call(["ifconfig",interface,"up"])
+
 parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest = "interface", help = "[+] Interface change it's MAC address")
+parser.add_option("-m", "--mac", dest = "new_mac", help = "[+] new MAC address")
+(options, arguments) = parser.parse_args()
 
-parser.add_option("-i", "--interface", dest="interface", help= "[+] Interface change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help= "[+] new MAC address")
-
-(options, arguments)=parser.parse_args()
-
-inter = options.interface
-new_mac = options.new_mac
-print("[+]" + inter + " is going to change with  " + new_mac)
-subprocess.call(["ifconfig", inter, "down"])
-subprocess.call(["ifconfig", inter, "hw","ether",new_mac])
-subprocess.call(["ifconfig", inter, "up"])
-
-
+mac_change(options.interface, options.new_mac)

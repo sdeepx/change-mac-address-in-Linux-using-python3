5c0fd6974a3639be0ddf832f24d0d0a5bb1f69fa
Shubhadeep <41886925+shubha161@users.noreply.github.com>
2019-01-04 14:41:00 +0530

    Update mac_ch.py

--- mac_ch.py
+++ mac_ch.py
@@ -1,17 +1,24 @@
 import subprocess
 import optparse
 
-def get_argument():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest = "interface", help = "[+] Interface change it's mac address")
-    parser.add_option("-m", "--mac", dest = "new_mac", help = "[+] new Mac address")
-    return parser.parse_args()
-
 def mac_change(interface, new_mac):
-    print("[+] your MAC address for " + interface + " to " + new_mac)
+    print("[+] Your MAC address changed for " + interface + " to " + new_mac)
+
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options, arguments) = get_argument()
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest = "interface", help = "[+] Interface changes it's mac address")
+    parser.add_option("-m", "--mac", dest = "new_mac", help = "[+] new MAC address")
+
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] please specify an interface, use '-h' or '--help' for more info")
+    if not options.new_mac:
+        parser.error("[-] please specify a mac, use '-h' or '--help' for more info")
+    return options
+
+options = get_args()
 mac_change(options.interface, options.new_mac)
